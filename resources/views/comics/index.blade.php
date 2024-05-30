0251d8fab5ebce3ddcb3d9b3ba1486af5ffc2803
EnricoClem <enrico.clem94@gmail.com>
2024-05-30 17:37:29 +0200

    ADD - index view

--- resources/views/comics/index.blade.php
+++ resources/views/comics/index.blade.php
@@ -1,1 +1,36 @@
-<h1>Vista: index</h1>
+@extends('layout.app')
+
+@section('title', 'Our comics!')
+
+@section('content')
+
+<main>
+    <div class="container">
+
+        <h1>Vista: index</h1>
+        
+        <table class="table">
+            <thead>
+                <th>Title</th>
+                <th>Price</th>
+                <th>Series</th>
+                <th>Release date</th>
+                <th></th>
+            </thead>
+            <tbody>
+                @foreach ($comics as $comic)
+                    <tr>
+                        <td>{{ $comic->title }}</td>
+                        <td>{{ $comic->price }}</td>
+                        <td>{{ $comic->series }}</td>
+                        <td>{{ $comic->sale_date }}</td>
+                    </tr>
+                @endforeach
+            </tbody>
+        </table>
+
+    </div>
+</main>
+
+
+@endsection
